1c8b8ed88c2d4b168d6cb991f656f3112a313f67
Clément <clement.diockel@gmail.com>
2026-08-13 04:11:30 +0200

    Ajout de updateEmployee, deleteEmployee et getEmployeeById

--- src/main/java/com/openclassrooms/api/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.openclassrooms.api.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +47,22 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Employé introuvable avec l'ID : " + id));
     }
 }
