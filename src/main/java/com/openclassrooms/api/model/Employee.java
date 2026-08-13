8605fd8dee3da081618babe807375b438c2adfb3
Clément <clement.diockel@gmail.com>
2026-08-13 04:41:47 +0200

    Mis à jour des classes Employee et EmployeeRepository

--- src/main/java/com/openclassrooms/api/model/Employee.java
+++ src/main/java/com/openclassrooms/api/model/Employee.java
@@ -25,6 +25,9 @@ public class Employee {
     @Column(name = "last_name")
     private String lastName;
 
-    private String mail;
+    @Column(name = "mail")
+    private String email;
+
+    @Column(name = "password")
     private String password;
 }
