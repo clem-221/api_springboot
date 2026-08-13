04a7e07d0d4dfe8572f81729866d689619614ef7
Clément <clement.diockel@gmail.com>
2026-08-13 03:34:57 +0200

    Ajout de @PostMapping dans EmployeeController.java afin qu'il puisse accepter la création d'un nouvel employé depuis l'Application web créé avec Spring Boot

--- src/main/java/com/openclassrooms/api/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.api.service.EmployeeService;
@@ -37,5 +39,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
     }
 }
